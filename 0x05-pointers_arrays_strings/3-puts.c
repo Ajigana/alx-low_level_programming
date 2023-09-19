3c2f28b18daf9bb714609e8c1d73f9270f48f328
Ajigana <talk4kelv@gmail.com>
2023-09-19 03:59:29 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+include "main.h"
+/**
+ *_puts - function name
+ *@str: function that prints a string
+ */
+
+void _puts(char *str)
+{
+	while (*str != *str++)
+	{	
+		_putchar (*str++);
+	}
+		:_putchar('\n');
+}
