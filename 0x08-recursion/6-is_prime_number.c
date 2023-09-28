c15d591f387e39fff3d1971d8f2d538a9172adad
Ajigana <talk4kelv@gmail.com>
2023-09-28 14:14:19 -0700

    100-is_palindrome.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,12 +8,10 @@ int check_prime(int n, int i);
 * @n: function parameter
 * Return: integer value
 */
-
 int is_prime_number(int n)
 {
 	return (check_prime(n, 1));
 }
-
 /**
 * check_prime - check if number is prime
 * @n: the number to be checked
@@ -25,13 +23,9 @@ int check_prime(int n, int i)
 {
 	if (n <= 1)
 		return (0);
-
 	if (n % i == 0 && i > 1)
 		return (0);
-
 	if ((n / i) < i)
 		return (1);
-
 	return (check_prime(n, i + 1));
-
 }
