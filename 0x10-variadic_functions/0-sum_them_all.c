a6f8ee0722b56d9d6b161d38bd827ea007d481ee
Ajigana <talk4kelv@gmail.com>
2023-10-14 22:49:37 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,10 +2,9 @@
 #include <stdarg.h>
 
 /**
- * sum_them_all - Returns the sum of all its paramters.
+ * sum_them_all - Returns the sum of all its paramters of the programme
  * @n: The number of paramters passed to the function.
- * @...: A variable number of paramters to calculate the sum of.
- *
+ * @...: A variable number of paramters to calculate the sum of
  * Return: If n == 0 - 0.
  *         Otherwise - the sum of all parameters.
  */
